d239747a8cb602b85a752a8cd160875c8618455c
hyuk-dev <hyuk.development@gmail.com>
2025-12-28 00:57:02 +0900

    refactor: 메인 레이아웃 분리

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Header from '@/components/header';
+import UserMenu from '@/components/userMenu';
+import React from 'react';
 
 export default function MainLayout({
   children,
 }: Readonly<{
-  children: React.ReactElement
+  children: React.ReactElement;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="flex flex-col items-center gap-5 max-w-[1580px] mx-auto">
+      <div className="flex justify-between w-full px-20 py-5 border-b border-b-gray-200">
+        <Header />
+        <UserMenu />
+      </div>
+      {children}
+    </div>
+  );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,14 +4,8 @@ import UserMenu from '@/components/userMenu';
 
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center gap-5 max-w-[1580px] mx-auto">
-      <div className="flex justify-between w-full px-20 py-5 border-b border-b-gray-200">
-        <Header />
-        <UserMenu />
-      </div>
-      <div className='grid-rows-2 w-full'>
-        <ProjectList />
-      </div>
+    <div className="grid-rows-2 w-full">
+      <ProjectList />
     </div>
   );
 }
